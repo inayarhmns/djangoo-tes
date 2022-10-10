910c6358850dbbbbf74207b02b11627077de13f6
inaya.rahmanisa <inaya.rahmanisa@gmail.com>
2022-10-10 19:43:07 +0700

    commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\inaya\OneDrive\Documents\kuliah\PBP\django_exercise\env
+set VIRTUAL_ENV=C:\Users\inaya\OneDrive\Documents\kuliah\PBP\djangoo-private\env
 
 if not defined PROMPT set PROMPT=$P$G
 
